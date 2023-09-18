38da46ba1d4260db2ad4d1b9dc0120518488686a
Salvador Medina-Ruiz <smedinar@emich.edu>
2023-09-18 13:59:23 -0400

    Added Database to Tech Stack Setup

--- src/personal/models.py
+++ src/personal/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. Where data structures are defined. Literally just make a class with a name variable. Make the list client side
+
+class Persons(models.Model):
+    name = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.name
--- src/personal/views.py
+++ src/personal/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render
-
+from .models import Persons
 # Create your views here.
 #context = {}
 #context['name'] = "Placeholder"
-list_of_names = []
+#list_of_names = []
 
 def home_screen_view(request):
     return render(request, "base.html", {}) #<-- {} for database variables
 
 def view1(request):
-    username = request.POST.get('name')
-    if username != None:
-        list_of_names.append(username)
-    context = {'username':username, 'list_of_names':list_of_names}
+    if request.method == 'POST':
+        post = Persons()
+        post.name = request.POST.get('name')
+        post.save()
+    
+    latest_person = Persons.objects.latest('id')
+
+    context = {'latest_person':latest_person}
     return render(request, "view1.html", context) #<-- {} for database variables
+    #username = request.POST.get('name')
+    #if username != None:
+    #    list_of_names.append(username)
+    #context = {'username':username, 'list_of_names':list_of_names}
+    #return render(request, "view1.html", context) #<-- {} for database variables
 
 def view2(request):
+    list_of_names = []
+    all_persons = Persons.objects.all
+    for item in all_persons():
+        if item.name != None:
+            list_of_names.append(item.name)
     context = {'list_of_names':list_of_names}
     return render(request, "view2.html", context) #<-- {} for database variables
